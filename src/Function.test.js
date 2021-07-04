1184bda1860342a1ea594ea0dd52cac8226a5049
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-07-04 15:59:28 +0900

    [ADD] Mock함수 이용 - 4

--- src/Function.test.js
+++ src/Function.test.js
@@ -4,20 +4,18 @@ const fn = require("./Function")
 // .mock 프로퍼티에 호출된 값이 고스란히 저장되 있다는 강점이 있음 
 // .calls로 알 수 있는 것은 함수가 총 몇 번 호출 됬는지와 호출될 때 전달 된 값을 알 수 있음
 // .results에는 return된 값이 배열로 들어있음
-const mockFn = jest.fn(num => num + 1);
+// ReturnValueOnce를 이용하여 실행할 때 마다 각각 다른 값을 넣어 줄 수 있음
+const mockFn = jest.fn();
 
-mockFn(10);
-mockFn(20);
-mockFn(30);
+mockFn
+    .mockReturnValueOnce(true)
+    .mockReturnValueOnce(false)
+    .mockReturnValueOnce(true)
+    .mockReturnValueOnce(false)
+    .mockReturnValue(true)
 
-test('10에서 1 증가한 값이 반환된다', () => {
-    expect(mockFn.mock.results[0].value).toBe(11);
-});
+const result = [1, 2, 3, 4, 5].filter(num => mockFn(num));
 
-test('20에서 1 증가한 값이 반환된다', () => {
-    expect(mockFn.mock.results[1].value).toBe(21);
-});
-
-test('30에서 1 증가한 값이 반환된다', () => {
-    expect(mockFn.mock.results[2].value).toBe(31);
-});
+test('홀수는 1,3,5 이다', () => {
+    expect(result).toStrictEqual([1, 3, 5]); // 배열을 확인할 땐 toStrictEqual을 사용해야 함 
+})
