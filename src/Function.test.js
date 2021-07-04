b5833cfee8722fee424b9ca16680fff8b9e3576f
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-07-04 15:32:21 +0900

    [ADD] Mock함수 이용 - 1

--- src/Function.test.js
+++ src/Function.test.js
@@ -1,70 +1,17 @@
 const fn = require("./Function")
 
-// beforeEach, afterEach 사용
-let num = 0;
+// mock function : 테스트하기 위해 흉내만 내는 함수
+// .mock 프로퍼티에 호출된 값이 고스란히 저장되 있다는 강점이 있음 
+// calls로 알 수 있는 것은 함수가 총 몇 번 호출 됬는지와 호출될 때 전달 된 값을 알 수 있음
+const mockFn = jest.fn();
 
-beforeEach(() => { // 이 함수는 각 테스트 직전에 실행됨
-    num = 0;
+mockFn(1);
+mockFn(2);
+
+test('함수는 2번 실행됨', () => {
+    expect(mockFn.mock.calls.length).toBe(2);
 })
 
-afterEach(() => { // 이 함수는 각 테스트 직후에 실행됨
-    num = 0;
+test('두 번째로 호출된 mock함수의 첫번째 인수 값은 2이다', () => {
+    expect(mockFn.mock.calls[1][0]).toBe(2);
 })
-
-test("0 + 1 = 1", () => {
-    num = fn.add(num, 1);
-    expect(num).toBe(1);
-})
-
-test("0 + 2 = 2", () => {
-    num = fn.add(num, 2);
-    expect(num).toBe(2);
-})
-
-// beforeAll, afterAll 사용
-let user;
-
-beforeAll(async () => { // 테스트 최초에 한 번 실행됨
-    user = await fn.connectUserDb();
-})
-
-afterAll(() => { // 테스트 최후에 한 번 실행됨
-    return fn.disconnectDb();
-})
-
-test("이름은 Taehwan", () => {
-    expect(user.name).toBe('Taehwan');
-});
-
-test("나이는 18", () => {
-    expect(user.age).toBe(18);
-});
-
-test("성별은 남성", () => {
-    expect(user.gender).toBe('Male');
-});
-
-// describe 사용
-describe("Car 관련 직업", () => {
-    let car;
-
-    beforeAll(async () => { // 테스트 최초에 한 번 실행됨
-        car = await fn.connectCarDb();
-    })
-
-    afterAll(() => { // 테스트 최후에 한 번 실행됨
-        return fn.disconnectCarDb();
-    })
-
-    test("차종은 'bmw'", () => {
-        expect(car.brand).toBe('bmw');
-    });
-
-    test.skip("자동차명은 'z4'", () => { // skip시 이 테스트를 스킵하고 실행시킴
-        expect(car.name).toBe('z4');
-    });
-
-    test.only("색상은 빨간색", () => { // only시 이 테스트만 실행 나머지 테스트는 스킵
-        expect(car.color).toBe('red');
-    });
-});
